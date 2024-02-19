68cb489c1d9694e9b9c6f7a8060034292412f908
Evan Kallas <144737982+egkallas@users.noreply.github.com>
2024-02-19 13:05:40 -0700

    Update code.js

--- code.js
+++ code.js
@@ -1,3 +1,51 @@
+function swap(arr, i, j) {
+    var temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+}
+
+function partition(array, position, start, end) {
+    if (start >= end) return start;
+
+    var l = start;
+    var r = end - 1;
+    var pivot = array[position];
+
+    while (l <= r) {
+        if (array[l] < pivot) {
+            l++;
+        }
+        else if (array[r] > pivot) {
+            r--;
+        }
+        else if (l <= r) {
+            swap(array, l, r);
+            l++;
+            r--;
+        }
+    }
+    return l;
+}
+
 function quicksort(array) {
+    var stack = [];
+    stack.push(0); 
+    stack.push(array.length); 
+
+    while (stack.length > 0) {
+        var end = stack.pop();
+        var start = stack.pop();
+
+        if (end - start < 2) {
+            continue;
+        }
+
+        var piv = partition(array, start, start, end);
+        stack.push(piv);
+        stack.push(end);
+        stack.push(start);
+        stack.push(piv);
+    }
+
     return array;
 }
